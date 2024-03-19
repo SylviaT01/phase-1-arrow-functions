b5de9001b6b3aa88f81c29ef4350a7b965c8e717
Sylvia <sylviachebet03@gmail.com>
2024-03-19 14:45:16 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = (parameter1 , parameter2) => parameter1/ parameter2;
+let parameter1 = 2000;
+let parameter2 = 100;
+divide(parameter1, parameter2);
+
+const square = x => x ** 2;
+let x = 2;
+square(x);
+
+const add = (a,b) => a + b;
+let a = 3;
+let b = 4;
+add(a, b);
